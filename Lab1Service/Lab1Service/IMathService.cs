c8762519d0915ca8b72c346e5d38708f3a2cec62
Lea Ann Bradford <leaann@notch8.com>
2020-06-27 13:25:58 -0700

    add lab 1 project files

---
+++ Lab1Service/Lab1Service/IMathService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Lab1Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMathService" in both code and config file together.
+    [ServiceContract]
+    public interface IMathService
+    {
+        [OperationContract]
+        double Add(double value1, double value2);
+
+        [OperationContract]
+        double Subtract(double value1, double value2);
+
+        [OperationContract]
+        double Multiply(double value1, double value2);
+
+        [OperationContract]
+        double Divide(double value1, double value2);
+
+        [OperationContract]
+        double CircleArea(double radius);
+    }
+}
---
+++ Lab1Service/TestClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using MathServiceRef;
+using System;
+
+namespace TestClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            MathServiceClient proxy = new MathServiceClient();
+            double result = await proxy.AddAsync(12.5, 2.3);
+            Console.WriteLine(result);
+
+            double subtractResult = await proxy.SubtractAsync(44.26, 22.13);
+            Console.WriteLine(subtractResult);
+
+            double multiplyResult = await proxy.MultiplyAsync(12.21, 21.12);
+            Console.WriteLine(multiplyResult);
+
+            double divideResult = await proxy.DivideAsync(144, 12);
+            Console.WriteLine(divideResult);
+
+            double circleAreaResult = await proxy.CircleAreaAsync(2.34);
+            Console.WriteLine(circleAreaResult);
+
+            Console.Write("Press <ENTER> quit...");
+            Console.ReadLine();
+        }
+    }
+}
